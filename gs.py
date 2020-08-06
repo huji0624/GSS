65cb2c0170173500d92aa683be26bf920619385a
huji <huji@hujis-MBP.lan>
2020-08-06 10:18:26 +0800

    f

--- gs.py
+++ gs.py
@@ -383,9 +383,8 @@ def update_all_data(web_all_data,now,k,d):
         rc['quote'] = d
         rc['hot'] = rc.get('hot',0) + 1
         web_all_data[k] = rc
-    if dlt > 30:
         import math
-        rc['hot'] = math.floor(rc.get('hot',0)/5*4*10)/10
+        rc['hot'] = math.floor(rc.get('hot',0)/10*9*10)/10
     return dlt
 
 def between_day_time(an,h1,m1,h2,m2):
