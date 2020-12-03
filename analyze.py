0c003d1ee31362793023b3c51c833948b22d1362
huji <huji@youloft.com>
2020-12-03 12:55:24 +0800

    change analyze.;

--- analyze.py
+++ analyze.py
@@ -35,52 +35,59 @@ def un_tar(file_name):
      tar.close()
      return to_name
 
-day_data = {}
-
 import sys
 import json
-tfile = sys.argv[1]
-with open(tfile,"r") as f:
-    t = f.read()
-    lines = t.split("\n")
-    for l in lines:
-        if "INFO:" in l:
-            if "uuid" in l:
-                dmap = json.loads(l.split("INFO:")[1].strip())
-                uid = dmap['uuid']
-                day = l[:10]
-                dd = day_data.get(day,{})
-                uid_v_count = dd.get(uid,0)
-                dd[uid] = uid_v_count+1
-                day_data[day] = dd
+import os
+import arrow
 
-keys = day_data.keys()
-keys.sort()
-last_dd = None
+logfiles = os.listdir("log")
+# logfiles = ["request.log.2020-12-01_00-00.log","request.log.2020-07-21_00-00.log","request.log.2020-11-27_00-00.log","request.log.2020-11-23_00-00.log",]
+
+logfiles = sorted(logfiles)
+logfiles = logfiles[-7:]
+logfiles.reverse()
 msgs = []
-for k in keys:
-    if "-" not in k:
-        continue
-    dd = day_data[k]
-    print(k + " : " + str(len(dd)))
-    visit_total = 0
-    last_day_user_cnt = 0.0
-    one_visit = 0
-    for d in dd:
-        vt = dd[d]
-        visit_total += vt
-        is_last_day_user = False
-        if vt==1:
-            one_visit += 1
-        if last_dd is not None and d in last_dd:
-            is_last_day_user = True
-            last_day_user_cnt += 1
-        msgs.append("  "+ d + " : "+str(vt)+"v "+str(vt*10/60)+"min - is_last_day_user: "+str(is_last_day_user))
-    msgs.append("  total visit : "+str(visit_total))
-    msgs.append("  av visit : "+str(visit_total/len(dd)))
-    msgs.append("  one visit count : "+str(one_visit)+" valide user : "+str(len(dd)-one_visit))
-    if last_dd is not None:
-        msgs.append("  ratio of last day user %f" % (last_day_user_cnt/len(last_dd)))
-    last_dd = dd
+msgs.append("Daily Report:")
+datas = {}
 
-notify_msg("\n".join(msgs))
+for tfile in logfiles:
+    day_data = {}
+    day = None
+    with open("log/"+tfile,"r") as f:
+        t = f.read()
+        lines = t.split("\n")
+        for l in lines:
+            if "INFO:" in l:
+                if "uuid" in l:
+                    dmap = json.loads(l.split("INFO:")[1].strip())
+                    uid = dmap['uuid']
+                    day = l[:10]
+                    dmap['day'] = day
+                    day_data[uid] = (dmap)
+    windows_count = 0
+    for uid in day_data:
+        it = day_data[uid]
+        if "Windows" in it['UA']:
+            windows_count+=1
+    
+    count = len(day_data)
+    windows_ratio = windows_count/count
+
+    mstr = "  %s uv:%3d windows:%.2f%%" % (day,count,windows_ratio*100)
+    next_day = arrow.get(day).shift(days=1)
+    next_day = next_day.format("YYYY-MM-DD")
+    
+    if next_day in datas:
+        nd = datas[next_day]
+        stay_count = len(list(set(day_data.keys()).intersection(set(nd.keys()))))
+        stay_ratio = stay_count/count
+        mstr += " 活跃次留:%.2f%%" % (stay_ratio*100)
+        
+    msgs.append(mstr)
+    datas[day] = day_data
+
+
+msg = "\n".join(msgs)
+# print(msg)
+# exit(1)
+notify_msg(msg)
